f08ac983923aa9e95e007fe037ca39fb3e5603f0
NepyAnna <nepyann@gmail.com>
2025-05-27 16:21:21 +0200

    add DBManager with sql connection

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,14 @@
 package org.example;
 
+import org.example.config.DBManager;
+
+import java.sql.Connection;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        DBManager.initConnection();
+        //DBManager.closeConnection();
     }
 }
