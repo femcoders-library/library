334c17e4ca1b71677ed0af7db56c1b913d34eb09
vitaFlash <vitaliaflash@gmail.com>
2025-05-29 21:36:56 +0200

    add: menu

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,9 @@
 package org.example;
-
-import org.example.config.DBManager;
 import org.example.controller.BookController;
 import org.example.repository.BookRepository;
 import org.example.view.BookView;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,11 +11,43 @@ public class Main {
         BookController bookController = new BookController(bookRepository);
         BookView bookView = new BookView(bookController);
 
+        Scanner scanner = new Scanner(System.in);
+        boolean running = true;
 
-        //bookView.createBook();
-        bookView.displayBooks();
-        //bookView.updateBook();
-        //bookView.deleteBook();
-        //bookView.updateBookByField();
+        while (running) {
+            System.out.println("\n=== Library Menu ===");
+            System.out.println("1. View all books");
+            System.out.println("2. Add a new book");
+            System.out.println("3. Edit a book");
+            System.out.println("4. Delete a book");
+            System.out.println("5. Exit");
+            System.out.print("Select an option: ");
+
+            String choice = scanner.nextLine();
+
+            switch (choice) {
+                case "1":
+                    bookView.displayBooks();
+                    break;
+                case "2":
+                    bookView.createBook();
+                    break;
+                case "3":
+                    // Uncomment the one you use
+                    // bookView.updateBook();
+                    bookView.updateBookByField();
+                    break;
+                case "4":
+                    bookView.deleteBook();
+                    break;
+                case "5":
+                    System.out.println("Goodbye!");
+                    running = false;
+                    break;
+                default:
+                    System.out.println("Invalid option. Please try again.");
+            }
+        }
+        scanner.close();
     }
 }
