d270f5543dde3fb0e70ffdb4626ff1b9275e7fcd
NepyAnna <nepyann@gmail.com>
2025-06-04 14:50:07 +0200

    add test to showManu in BookView

--- src/test/java/org/example/view/BookViewTest.java
+++ src/test/java/org/example/view/BookViewTest.java
@@ -8,24 +8,27 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class BookViewTest {
-
     @Mock
     private BookController bookController;
 
     private Scanner testScanner;
     private BookView bookView;
 
-    @BeforeEach
-    void setUp() {
+    @Test
+    void updateBookByField_shouldCallControllerWithCorrectUpdates() {
         String input = String.join("\n",
                 "1234567890",
                 "title",
@@ -34,10 +37,7 @@ public class BookViewTest {
         );
         testScanner = new Scanner(new ByteArrayInputStream(input.getBytes()));
         bookView = new BookView(bookController, testScanner);
-    }
 
-    @Test
-    void updateBookByField_shouldCallControllerWithCorrectUpdates() {
         when(bookController.existByISBN("1234567890")).thenReturn(true);
 
         bookView.updateBookByField();
@@ -46,5 +46,32 @@ public class BookViewTest {
         expected.put("title", "New Title");
 
         verify(bookController).updateBookByField("1234567890", expected);
+    }
+
+    @Test
+    void showMenu_shouldManageInvalidInput(){
+        InputStream originalIn = System.in;
+        PrintStream originalOut = System.out;
+        String simulatedInvalidInput = "abc\n8\n";
+
+        ByteArrayInputStream testIn = new ByteArrayInputStream(simulatedInvalidInput.getBytes());
+        ByteArrayOutputStream testOut = new ByteArrayOutputStream();
+
+        try {
+            System.setIn(testIn);
+            System.setOut(new PrintStream(testOut));
+
+            bookView = new BookView(bookController, new Scanner(System.in));
+
+            bookView.showMenu();
+
+            String output = testOut.toString();
+
+            assertTrue(output.contains("Invalid option. Please try again"));
+            assertTrue(output.contains("Bye!"));
+        } finally {
+            System.setIn(originalIn);
+            System.setOut(originalOut);
+        }
     }
 }
